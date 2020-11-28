2b9adba5a01345acf91c474fa18c50dcecf76d27
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-11-28 22:58:14 +0530

    import pygame library

--- snowfalling.py
+++ snowfalling.py
@@ -1,9 +1,16 @@
 import pygame
 import random
+
 pygame.init()
 
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-myDisplay = pygame.display.set_mode((1110,600))
+myDisplay = pygame.display.set_mode((1110, 600))
 pygame.display.set_caption("Snow_Falling")
+
+SonwFlakes = []
+for q in range(100):
+    x = random.randrange(0, 1110)
+    y = random.randrange(0, 590)
+    SonwFlakes.append([x, y])
