24787a97f635b31e897b904f9ef9deadbefd1780
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-01 13:00:25 +0530

    background img added

--- snowfalling.py
+++ snowfalling.py
@@ -9,8 +9,22 @@ white = (255, 255, 255)
 myDisplay = pygame.display.set_mode((1110, 600))
 pygame.display.set_caption("Snow_Falling")
 
-SonwFlakes = []
+SnowFlakes = []
 for q in range(100):
     x = random.randrange(0, 1110)
     y = random.randrange(0, 590)
-    SonwFlakes.append([x, y])
+    SnowFlakes.append([x, y])
+
+clock = pygame.time.Clock()
+snow = False
+background_position = [0, 0]
+background_image=pygame.image.load("Background.jpeg").convert()
+
+while not snow:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            snow = True
+
+    myDisplay.blit(background_image,background_position)
+
+
